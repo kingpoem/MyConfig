194ba8a63cc1784c37d1eb7648bb3ed65f9269ed
poem <junqiwang93@gmail.com>
2024-12-13 18:36:21 +0800

    feat: add .gitconfig

--- scripts.sh
+++ scripts.sh
@@ -2,5 +2,6 @@
 cat ~/.zshrc > ~/Applications/vscode/github/MyConfig/zsh_/.zshrc
 cat ~/.tmux.conf > ~/Applications/vscode/github/MyConfig/tmux_/.tmux.conf
 cat ~/tmux_conf_bak > ~/Applications/vscode/github/MyConfig/tmux_/tmux_conf_bak
+cat ~/.gitconfig > ~/Applications/vscode/github/MyConfig/git_/.gitconfig
 
 
