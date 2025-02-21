0729960cd2374496bbd3edd9e88fd3327a541be7
poem <1322153392@qq.com>
2025-02-21 16:10:11 +0800

    refactor: add interactive logic

--- Readme.md
+++ Readme.md
@@ -1,29 +1,22 @@
 # Dot configuration
 
-这是我的 Dot 配置文件管理仓库。通过 `init.sh` 和 `update.sh` 脚本可以实现自动更新配置。所有配置文件路径尽量按照官方和默认配置放置。
+这是我的 Dot 配置文件管理仓库。通过 `config.sh` 脚本可以实现自动更新配置。所有配置文件路径尽量按照官方和默认配置放置。
 
 ## 使用方法
 
-`init.sh` 和 `update.sh` 脚本将根据您的主机名自动创建一个 git 分支供您自行配置。
+`config.sh` 脚本将根据您的主机名自动创建一个 git 分支供您自行配置。
 在该仓库根目录下，通过运行
 ```bash
-bash init.sh dot-config-name
+bash ./config.sh -i|u -d dot-config-name
 ```
-即可将项目中的默认配置文件软链接到您的相应配置文件上。
-
-通过运行
-```bash
-bash update.sh dot-config-name
-```
-即可将您自己的配置文件更新到仓库中，供您管理。
-
-目前支持的 `dot-config-name` 如下：
-
-- git
-- lvim
-- tmux
-- zsh
-- all 更新所有在上面提到的配置文件
+- `-i` 将项目中的默认配置文件软链接到您的相应配置文件上。
+- `-u` 将您自己的配置文件更新到仓库中，供您管理。
+- `-d` 指定 dot-config-name，可选值如下：
+    - git
+    - lvim
+    - tmux
+    - zsh
+    - all 更新所有在上面提到的配置文件
 
 如您有其他需求，欢迎提 issue，或者您可以自行修改脚本。
 
@@ -33,7 +26,7 @@ bash update.sh dot-config-name
 
 本仓库仅处理全局配置文件，git 配置文件路径如下：
 - `/etc/gitconfig` 系统配置文件
-- `~/.gitconfig` 全局配置文件
+- `~/.gitconfig` 用户配置文件
 - `.git/config` 局部配置文件
 
 ### lvim
@@ -58,6 +51,11 @@ bash update.sh dot-config-name
 
 相关配置参考：
 [Oh My Zsh][ohmyzsh]
+
+### gdb
+
+- `/etc/gdb/gdbinit` 系统配置文件
+- `~/.gdbinit` 用户配置文件
 
 ### nvim
 
---
+++ config.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# set -eux
+
+declare -r HOSTNAME=$(hostname)
+declare -r CURRENT_BRANCH=$(git branch --show-current)
+declare -r ROOT_DIR=$(git rev-parse --show-toplevel)
+declare -x DIR="all"
+declare -x FILE=""
+
+declare -x -a -r git_config=("git_" ".gitconfig")
+declare -x -a -r lvim_config=("lvim_" ".config/lvim/config.lua" ".config/lvim/lazy-lock.json")
+declare -x -a -r tmux_config=("tmux_" ".tmux.conf")
+declare -x -a -r zsh_config=("zsh_" ".zshrc")
+
+function init() {
+    local dir="$1"
+    if [ ! -d "$1" ]; then
+        mkdir $1
+    fi
+    ln -s ~/${2} $(git rev-parse --show-toplevel)/${1}/$( basename ${2} )
+    echo -e "\e[32mInitializing $2 configuration successfully!\e[0m"
+}
+
+function update() {
+    local dir="$1"
+    if [ ! -d "$1" ]; then
+        mkdir $1
+    fi
+    cp -i ~/${2} ${ROOT_DIR}/${1}/$( basename ${2} )
+    echo -e "\e[32mUpdating $2 configuration successfully!\e[0m"
+}
+
+# check if it is a git repository
+if [ ! -d ".git" ]; then
+    echo "\e[31mThis is not a git repository, please run this script in the root directory of your configuration!\e[0m"
+    exit 1
+fi
+
+# ==================================Git Branch Management==================================================
+if git show-ref --verify --quiet refs/heads/"$HOSTNAME"; then
+    if [ "$CURRENT_BRANCH" != "$HOSTNAME" ]; then
+        git switch "$HOSTNAME"
+        echo -e "\e[31mBranch `$HOSTNAME` already exists, switch to it successfully!\e[0m"
+        echo -e "\e[31mIf you want to name it differently, please remove this block in pre.sh and run `git branch -m $CURRENT_BRANCH your_branch_name` manually!\e[0m"
+    fi
+else
+    git switch -c $HOSTNAME
+    echo -e "\e[31mBranch `$HOSTNAME` is created and switch to it successfully!\e[0m"
+fi
+# ==========================================================================================================
+
+while getopts "iud::" opt; do
+    case ${opt} in
+        i)
+            declare -x -r TAG="init"
+            ;;
+        u)
+            declare -x -r TAG="update"
+            ;;
+        d)
+            DIR=${OPTARG}
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [[ "$DIR" == "all" ]]; then
+    echo -e "\e[32mAll files will ${TAG}!\e[0m"
+elif [[ "$DIR" == "git" ]]; then
+    echo -e "\e[32m请选择要 ${TAG} 的 git 配置文件：\e[0m"
+    echo -e "\e[32m1. .gitconfig\e[0m"
+    read -p "请输入序号：" num
+
+    for (( i=0; i<${#num}; i++ )); do
+        char="${num:$i:1}"
+        case $char in 
+            1)
+                ${TAG} "${git_config[0]}" "${git_config[1]}"
+                ;;
+            *)
+                echo "Invalid option: $char" >&2
+                exit 1
+                ;;
+        esac
+    done
+elif [[ "$DIR" == "lvim" ]]; then
+    echo -e "\e[32m请选择要 ${TAG} 的 lvim 配置文件：\e[0m"
+    echo -e "\e[32m1. .config/lvim/config.lua\e[0m"
+    echo -e "\e[32m2. .config/lvim/lazy-lock.json\e[0m"
+    read -p "请输入序号：" num
+
+    for (( i=0; i<${#num}; i++ )); do
+        char="${num:$i:1}"
+        case $char in 
+            1)
+                ${TAG} "${lvim_config[0]}" "${lvim_config[1]}"
+                ;;
+            2)
+                ${TAG} "${lvim_config[0]}" "${lvim_config[2]}"
+                ;;
+            *)
+                echo "Invalid option: $char" >&2
+                exit 1
+                ;;
+        esac
+    done
+elif [[ "$DIR" == "tmux" ]]; then
+    echo -e "\e[32m请选择要 ${TAG} 的 tmux 配置文件：\e[0m"
+    echo -e "\e[32m1. .tmux.conf\e[0m"
+    read -p "请输入序号：" num
+
+    for (( i=0; i<${#num}; i++ )); do
+        char="${num:$i:1}"
+        case $char in 
+            1)
+                ${TAG} "${tmux_config[0]}" "${tmux_config[1]}"
+                ;;
+            *)
+                echo "Invalid option: $char" >&2
+                exit 1
+                ;;
+        esac
+    done
+elif [[ "$DIR" == "zsh" ]]; then
+    echo -e "\e[32m请选择要 ${TAG} 的 zsh 配置文件：\e[0m"
+    echo -e "\e[32m1. .zshrc\e[0m"
+    read -p "请输入序号：" num
+
+    for (( i=0; i<${#num}; i++ )); do
+        char="${num:$i:1}"
+        case $char in 
+            1)
+                ${TAG} "${zsh_config[0]}" "${zsh_config[1]}"
+                ;;
+            *)
+                echo "Invalid option: $char" >&2
+                exit 1
+                ;;
+        esac
+    done
+else
+    echo "Invalid option: $DIR" >&2
+    echo "Please choose from: git, lvim, tmux, zsh"
+    exit 1
+fi
